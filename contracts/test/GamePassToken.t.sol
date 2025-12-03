e5e119856377f7890a0b208ee69a397a4baec920
dev.uche <uchennahanson@gmail.com>
2025-12-03 17:43:46 +0100

    test: create test file structure

---
+++ contracts/test/GamePassToken.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {GamePassToken} from "../src/GamePassToken.sol";
+
+contract GamePassTokenTest is Test {
+    GamePassToken public token;
+    
+    address public owner = address(1);
+    address public treasury = address(2);
+    address public rewardsContract = address(3);
+    address public swapContract = address(4);
+    address public user1 = address(5);
+    address public user2 = address(6);
+    
+    uint256 constant MAX_SUPPLY = 1_000_000_000 * 10**18;
+    uint256 constant TREASURY_INITIAL_SUPPLY = 500_000_000 * 10**18;
+    
+    function setUp() public {
+        vm.startPrank(owner);
+        
+        token = new GamePassToken(
+            "GamePass Token",
+            "PASS",
+            treasury
+        );
+        
+        vm.stopPrank();
+    }
